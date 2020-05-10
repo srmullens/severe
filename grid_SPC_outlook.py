c47f7f42cb1c778ce3a200d42f4e63b6c32f4e70
Stephen Mullens <55811438+srmullens@users.noreply.github.com>
2020-05-09 22:58:04 -0400

    Add files via upload

--- grid_SPC_outlook.py
+++ grid_SPC_outlook.py
@@ -41,10 +41,13 @@ from dateutil import tz
 
 
 # What area do you want to plot? 'data', 'CONUS', 'Southeast', 'Florida'
-where='Florida'
+where='data'
 
 # What type of plot: 'exact' or 'smooth'
 plot_type = 'smooth'
+
+# What SPC day do you want to plot?
+plot_day = 1
 
 # What resolution do you want? 'low', 'mid', 'high'
 setting = 'low'
@@ -276,9 +279,14 @@ def Great_Lakes():
 big_start_timer = dt.now()
 print('--> Get the files')
 
-shapefiles = {
-    "spc": {"categorical_day1": "https://www.spc.noaa.gov/products/outlook/day1otlk-shp.zip"}
-            }
+if plot_day<4:
+    shapefiles = {
+        "spc": {f"categorical_day{plot_day}": f"https://www.spc.noaa.gov/products/outlook/day{plot_day}otlk-shp.zip"}
+                }
+else:
+    shapefiles = {
+        "spc": {f"categorical_day{plot_day}": f"https://www.spc.noaa.gov/products/exper/day4-8/day{plot_day}prob-shp.zip"}
+                }
 """
 shapefiles = {
     "spc": {
@@ -306,7 +314,8 @@ shapefiles = {
 """
 
 # Clear directories before getting data
-target_folders = ['nhc', 'spc', 'wpc', 'drought', 'zips']
+target_folders = ['spc', 'zips']
+#target_folders = ['nhc', 'spc', 'wpc', 'drought', 'zips']
 for folder in target_folders:
     if not os.path.exists(folder):
         os.makedirs(folder)
@@ -327,7 +336,15 @@ for zip_file in glob.glob(f"{os.getcwd()}/*.zip"):
 
 
 # Read in Shapefile
-cat_gdf = geopandas.read_file('spc/day1otlk-shp/day1otlk_cat.shp')
+if plot_day<4: cat_gdf = geopandas.read_file(f'spc/day{plot_day}otlk-shp/day{plot_day}otlk_cat.shp')
+else: cat_gdf = geopandas.read_file(f'spc/day{plot_day}prob-shp/day{plot_day}otlk_{big_start_timer:%Y%m%d}_prob.shp')
+
+# Plot low categories as exact polygons,
+#   regardless of the original setting.
+if plot_day<4 and cat_gdf.iloc[2:5].empty:
+    plot_type = 'exact'
+else: plot_type = 'smooth'
+
 
 time_elapsed = dt.now() - start_timer
 tsec = round(time_elapsed.total_seconds(),2)
@@ -345,8 +362,16 @@ cat_plot_colors = {'General Thunderstorms Risk':'#C1E9C1',
                    'Enhanced Risk': '#FFA366',
                    'Moderate Risk': '#E06666',
                    'High Risk': '#e29ee9'}
+prob_plot_colors = {'15% Any Severe Risk':'#FFE066',
+                    '30% Any Severe Risk':'#FFA366'}
 
-cat_fill_colors = ['white','#C1E9C1','#66A366','#FFE066','#FFA366','#E06666','magenta']
+cat_fill_colors = [(1,1,1,0),'#C1E9C1','#66A366','#FFE066','#FFA366','#E06666','magenta']
+prob_fill_colors = [(1,1,1,0),'#FFE066','#FFA366']
+
+if plot_day>3:
+    cat_plot_colors = prob_plot_colors
+    cat_fill_colors = prob_fill_colors
+
 """
 # Set colors
 cat_plot_colors = {'Marginal Risk': 'green',
@@ -391,8 +416,8 @@ elif where=='data':
     elif len(extents)==1:
         # Plot extent of only category.
         extent = extents[0]
-        clat = (extent[0]+extent[1])/2
-        clon = (extent[2]+extent[3])/2
+        clon = (extent[0]+extent[1])/2
+        clat = (extent[2]+extent[3])/2
         map_crs = ccrs.Orthographic(central_latitude=clat, central_longitude=clon)
         leg_loc = 4
     else:
@@ -408,95 +433,98 @@ if leg_loc==4:
     att_x = 0.006; att_y = 0.01; att_ha='left'
 
 
+
 #
 # STEP 3: Make the lat/lon grid.
 #
 
-start_timer = dt.now()
-print('--> Make grid')
+if plot_type == 'smooth':
+    start_timer = dt.now()
+    print('--> Make grid')
 
-# Make a grid of 0.01x0.01 degrees that covers the CONUS.
-#   CONUS covers N,S,W,E: 50,24,-125,-66
+    # Make a grid of 0.01x0.01 degrees that covers the CONUS.
+    #   CONUS covers N,S,W,E: 50,24,-125,-66
 
-if setting=='high':
-    y, x = grid_points = np.mgrid[24:50:2601j, -125:-66:5901j]
-    US_mask_count = 8357331     # 54.45%
-    grid_size = 15348501
-elif setting=='mid':
-    y, x = grid_points = np.mgrid[24:50:521j, -125:-66:1181j]
-    US_mask_count = 334094      # 54.30%
-    grid_size = 615301
-elif setting=='low':
-    y, x = grid_points = np.mgrid[24:50:261j, -125:-66:591j]
-    US_mask_count = 83477       # 54.11%
-    grid_size = 154251
+    if setting=='high':
+        y, x = grid_points = np.mgrid[24:50:2601j, -125:-66:5901j]
+        US_mask_count = 8357331     # 54.45%
+        grid_size = 15348501
+    elif setting=='mid':
+        y, x = grid_points = np.mgrid[24:50:521j, -125:-66:1181j]
+        US_mask_count = 334094      # 54.30%
+        grid_size = 615301
+    elif setting=='low':
+        y, x = grid_points = np.mgrid[24:50:261j, -125:-66:591j]
+        US_mask_count = 83477       # 54.11%
+        grid_size = 154251
 
-#
-# Get the US's shape from natural_earth's countries.
-#   Helps speed up the smoothing process.
-#
+    #
+    # Get the US's shape from natural_earth's countries.
+    #   Helps speed up the smoothing process.
+    #
 
-shpfilename = shpreader.natural_earth(resolution='10m',
-                                      category='cultural',
-                                      name='admin_0_countries')
-reader = shpreader.Reader(shpfilename)
-countries = list(reader.records())
+    shpfilename = shpreader.natural_earth(resolution='10m',
+                                          category='cultural',
+                                          name='admin_0_countries')
+    reader = shpreader.Reader(shpfilename)
+    countries = list(reader.records())
 
-US, = [country for country in countries if country.attributes['NAME_EN'] == 'United States of America']
-US_geom = US.geometry
+    US, = [country for country in countries if country.attributes['NAME_EN'] == 'United States of America']
+    US_geom = US.geometry
 
-# Mask of the United States.
-US_mask = shapely.vectorized.contains(US_geom, x, y)
+    # Mask of the United States.
+    US_mask = shapely.vectorized.contains(US_geom, x, y)
 
 
-time_elapsed = dt.now() - start_timer
-tsec = round(time_elapsed.total_seconds(),2)
-print(f"  --> Grid made ({tsec:.2f} seconds)")
+    time_elapsed = dt.now() - start_timer
+    tsec = round(time_elapsed.total_seconds(),2)
+    print(f"  --> Grid made ({tsec:.2f} seconds)")
 
 
 
 #
 # STEP 4: Determine which grid points are in the outlook.
 #
-print('--> Mask SPC categories')
+if plot_type == 'smooth':
+    print('--> Mask SPC categories')
 
-# Create grid value for each SPC category
-# 0=N/A, 1=TSTM, ... , 6=HIGH
-categories = []
+    # Create grid value for each SPC category
+    # 0=N/A, 1=TSTM, ... , 6=HIGH
+    categories = []
 
-for key in cat_plot_colors.keys():
-    geometries = cat_gdf[cat_gdf['LABEL2'] == key]
+    for key in cat_plot_colors.keys():
+        geometries = cat_gdf[cat_gdf['LABEL2'] == key]
 
-    if len(geometries) > 0:
-        row = geometries.index[0]
-        print(f'  --> {key}: {geometries.at[row,"fill"]}')
+        if len(geometries) > 0:
+            row = geometries.index[0]
+            print(f'  --> {key}: {geometries.at[row,"fill"]}')
 
-        # Get the polygon or multipolygon for this category.
-        cat_geom = geometries.at[row,'geometry']
+            # Get the polygon or multipolygon for this category.
+            cat_geom = geometries.at[row,'geometry']
 
-        # Make TRUE/FALSE grid for whether location is in the polygon.
-        cat_mask = shapely.vectorized.contains(cat_geom, x, y)
+            # Make TRUE/FALSE grid for whether location is in the polygon.
+            cat_mask = shapely.vectorized.contains(cat_geom, x, y)
 
-        # Turn False=0 and True=category value.
-        cat_mask = cat_mask.astype(int)*(row+1)
+            # Turn False=0 and True=category value.
+            cat_mask = cat_mask.astype(int)*(row+1)
 
-        # Gather the 1D arrays for all the categories.
-        categories.append(cat_mask)
+            # Gather the 1D arrays for all the categories.
+            categories.append(cat_mask)
 
-category = []
-for i in categories:
-    if len(category)==0: category = i
-    else: category = np.add(category,i)
+    category = []
+    for i in categories:
+        if len(category)==0: category = i
+        else: category = np.add(category,i)
 
-# Output results.
-for i in range(7):
-    if i==0:
-        print(f'\nNONE: {(category == i).sum()} = {(category == i).sum()/grid_size*100:.2f}% of Grid')
-    elif i<=len(cat_gdf):
-        label2 = [key for key in cat_plot_colors.keys()][i-1]
-        label = list(cat_gdf[cat_gdf['LABEL2'] == label2]['LABEL'])[0]
-        print(f'{label}: {(category >= i).sum()} = {(category >= i).sum()/US_mask_count*100:.2f}% of US')
-print('\n')
+    # Output results.
+    for i in range(7):
+        if i==0:
+            print(f'\nNONE: {(category == i).sum()} = {(category == i).sum()/grid_size*100:.2f}% of Grid')
+        elif i<=len(cat_gdf):
+            label2 = [key for key in cat_plot_colors.keys()][i-1]
+            label = list(cat_gdf[cat_gdf['LABEL2'] == label2]['LABEL'])[0]
+            print(f'{label}: {(category >= i).sum()} = {(category >= i).sum()/US_mask_count*100:.2f}% of US')
+    print('\n')
 
 
 
@@ -538,12 +566,15 @@ legend_patches = []
 for i,risk in enumerate(reversed(list(cat_plot_colors.keys()))):
     if risk == 'General Thunderstorms Risk':
         patch = mpatches.Patch(color=cat_plot_colors[risk], label='Lightning Storms')
-    else:
+    elif plot_day<4:
         patch = mpatches.Patch(color=cat_plot_colors[risk], label=f'{5-i}: {risk}')
+    else:
+        patch = mpatches.Patch(color=cat_plot_colors[risk], label=f'{risk}')
     legend_patches.append(patch)
 
 # Setup matplotlib figure
-fig = plt.figure(1, figsize=(1024/96, 512/96))
+#fig = plt.figure(1, figsize=(1024/96, 512/96))
+fig = plt.figure(1, figsize=(1024/48, 512/48))
 ax = plt.subplot(1, 1, 1, projection=map_crs)
 
 
@@ -552,8 +583,7 @@ print("--> Plot SPC polygons")
 # Create colormap
 cmap_name='SPC'
 n_bin = 100
-cm = LinearSegmentedColormap.from_list(
-        cmap_name, cat_fill_colors, N=n_bin)
+cm = LinearSegmentedColormap.from_list(cmap_name, cat_fill_colors, N=n_bin)
 
 # Plot the categories
 if plot_type=='smooth':
@@ -565,21 +595,24 @@ if plot_type=='smooth':
     pm = ax.pcolormesh(x,y,category,**kwargs)
 
 elif plot_type=='exact':
-    for key in cat_plot_colors.keys():
+
+    for i,key in enumerate(cat_plot_colors.keys()):
         geometries = cat_gdf[cat_gdf['LABEL2'] == key]
+
         # Check to see if there an area outlooked at all. If so, add the polygons to the map.
         if len(geometries) > 0:
             ax.add_geometries(geometries['geometry'], crs=data_crs,
-                              facecolor=cat_plot_colors[key],
-                              alpha=0.5)
+                          facecolor=cat_fill_colors[i+1],alpha=1)
 
 # Set plot extent
 ax.set_extent(extent, data_crs)
 
 # Add map features
 print("--> Adding cfeatures")
-ax.add_feature(cfeature.OCEAN.with_scale('50m'),zorder=2,edgecolor='k')
-ax.add_feature(cfeature.LAND.with_scale('50m'))
+#ax.add_feature(cfeature.OCEAN.with_scale('50m'))
+if plot_type=='exact': ax.add_feature(cfeature.OCEAN.with_scale('50m'))
+elif plot_type=='smooth': ax.add_feature(cfeature.OCEAN.with_scale('50m'),zorder=2,edgecolor='k')
+ax.add_feature(cfeature.LAND.with_scale('50m'),facecolor='w')
 ax.add_feature(cfeature.COASTLINE.with_scale('50m'))
 ax.add_feature(cfeature.STATES.with_scale('50m'))
 
@@ -591,12 +624,12 @@ for lake in great_lakes:
 print("--> Legend, Title")
 # Plot the legend
 kwargs = {'loc':leg_loc,
-            'fontsize':'small'
+            'fontsize':'medium'
          }
 plt.legend(handles=legend_patches,**kwargs).set_zorder(7)
 
 # Plot the titles
-plt.suptitle('SPC Day 1 Severe Storm Outlook', fontsize=18, fontweight='bold')
+plt.suptitle('SPC Day 1 Severe Storm Outlook', fontsize=18, fontweight='bold', y=0.95)
 plt.title(f'{start_time} through {end_time}', fontsize=12, loc='center')
 
 # Add attribution
@@ -621,7 +654,10 @@ print(f"--> Map made ({tsec:.2f} seconds)")
 print("--> Save")
 start_timer = dt.now()
 
-plt.savefig('spc/day1_grid_categorical.png', dpi=96, bbox_inches='tight')
+if plot_type=='exact':
+    plt.savefig(f'spc/day{plot_day}_categorical.png', dpi=96, bbox_inches='tight')
+elif plot_type=='smooth':
+    plt.savefig(f'spc/day{plot_day}_grid_categorical.png', dpi=96, bbox_inches='tight')
 
 time_elapsed = dt.now() - start_timer
 tsec = round(time_elapsed.total_seconds(),2)
