5a57c83826c147b1384ca8b02a78a6c066474e44
Stephen Mullens <55811438+srmullens@users.noreply.github.com>
2020-05-13 13:30:53 -0400

    Adjusted for testing

--- grid_SPC_outlook.py
+++ grid_SPC_outlook.py
@@ -868,7 +868,8 @@ if __name__ == "__main__":
     if time.hour in [1,12,13,16,20]: h1=1; h2=2
     elif time.hour in [17]: h1=2; h2=3
     elif time.hour in [7]: h1=2; h2=4
-    else: h1=4; h2=9
+    else: h1=1; h2=2
+    #else: h1=4; h2=9
 
     for plot_day in range(h1,h2):
         print(f"\n*** Day {plot_day} ***")
