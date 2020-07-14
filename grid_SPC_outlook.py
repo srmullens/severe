e1e6df88d94c29561b37106a5e58b8efac5d1704
Stephen Mullens <55811438+srmullens@users.noreply.github.com>
2020-07-14 13:14:00 -0400

    import Cartopy

--- grid_SPC_outlook.py
+++ grid_SPC_outlook.py
@@ -65,6 +65,7 @@ from twython import Twython
 
 # Configuration lines for Cartopy.
 # Trying to prevent it from downloading Natural Earth data during the figure saving process.
+import cartopy
 cartopy.config['data_dir']='/home/user/.local/map_features'
 cartopy.config['pre_existing_data_dir']='/home/user/.local/map_features'
 
